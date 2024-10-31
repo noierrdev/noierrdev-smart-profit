70cc495eec2ca9d1f17e31424013385aa2d618a5
vander moleker <vandermoleker@gmail.com>
2024-10-31 08:33:17 -0700

    update

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const WebSocket = require('ws');
 const { pumpfunSwapTransactionFaster, swapTokenAccounts, swapPumpfunFaster, swapTokenFastest, swapTokenFastestWallet, pumpfunSwapTransactionFasterWallet, swapTokenAccountsWallet, swapPumpfunFasterWallet } = require("./swap");
 const { getAssociatedTokenAddressSync } = require("@solana/spl-token");
 
-const {Bot,Context,session}=require("grammy");
 const { getSwapMarket, getSwapMarketFaster } = require("./utils");
 const Client=require("@triton-one/yellowstone-grpc");
 const bs58=require("bs58")
