d13e6f54d941f6ea13b95bca1de60f154808f2a3
vander moleker <vandermoleker@gmail.com>
2024-11-15 00:41:16 -0800

    update

--- index.js
+++ index.js
@@ -257,7 +257,7 @@ function connectGeyser(){
                         if(!detected) return;
                         const signers=[allAccounts[0]]
                         if(allAccounts.includes(PUMPFUN_BONDINGCURVE)){
-                            if(transaction.meta.logMessages.includes("Program log: Instruction: InitializeMint2")){
+                            if(transaction.meta.logMessages.some(log=>log.includes("InitializeMint2"))){
                                 console.log(transaction)
                             }
                         }
